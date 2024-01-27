779d25913e67e860afb16f0a86d985593428185a
MitrichMD <151858938+MitrichMD@users.noreply.github.com>
2024-01-27 21:34:14 +0300

    Update test_items.py
    add assert

--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-import pytest
+import selenium.common
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -8,7 +8,7 @@ def test_page_has_addtobasket_button(browser):
     try:
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".btn.btn-add-to-basket")
-    except:
-        pytest.fail("Кнопка не найдена")
+    except selenium.common.NoSuchElementException:
+        assert False, "Кнопка не найдена"
     finally:
         time.sleep(5)
