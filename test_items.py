7a1825ea58b8710cad4e992c27162d1d2e06c442
MitrichMD <the.mitrich.himself@gmail.com>
2024-01-30 21:31:16 +0300

    rename unused conftest files

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_page_has_addtobasket_button(browser):
     try:
         browser.get(link)
-        browser.find_element(By.CSS_SELECTOR, ".btn.btn-add-to-basket")
+        browser.find_element(By.CSS_SELECTOR, ".btn.btn-add-to-baske")
     except selenium.common.NoSuchElementException:
         assert False, "Кнопка не найдена"
     finally:
